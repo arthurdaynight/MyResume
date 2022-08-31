bac2325761cc4bd837941eea9901d86444fc69e6
arthurdaynight <arthurdaynight@gmail.com>
2022-08-31 13:31:26 -0400

    So this is a much better less bugy version, nothing on the writen in the resume becuase I left that part for last

--- src/sun.js
+++ src/sun.js
@@ -5,7 +5,7 @@ export default function Sun(){
     return(
         <div className="columns">
             
-            <img  className="sun" src="https://www.drawingwars.com/assets/img/cartoons/how-to-draw-a-sun-step-by-step/how-to-draw-a-sun-step-by-step_transparent.png?ezimgfmt=ng%3Awebp%2Fngcb1"/>
+            <img  className="sun" src="https://i.ibb.co/jD6Nk7j/how-to-draw-a-sun-step-by-step-transparent.png"/>
             <img  className="cloud1" src="https://i.ibb.co/KhWykby/dpngtree-cartoon-cloud-free-buckle-decorative-element-png-image-4460066-2.png"/>
             <img className="cloud2" src="https://i.ibb.co/dQpkbJM/pngtree-cartoon-cloud-free-buckkle-decorative-element-png-image-4460066-1.png"/>
             <img className="cloud3" src="https://i.ibb.co/dQpkbJM/pngtree-cartoon-cloud-free-buckkle-decorative-element-png-image-4460066-1.png"/>
