fe554e0ee55400bf9cab4a032d5152d12cb7a1b1
arthurdaynight <arthurdaynight@gmail.com>
2022-08-31 18:17:46 -0400

    Fixing more production bugs

--- src/Resume.js
+++ src/Resume.js
@@ -8,11 +8,6 @@ export default function Resume() {
                 <div className="secretColumn"></div>
         <div>
         <img className="whiteFrame" src="https://i.ibb.co/qg4nykg/Untitled-1.png" alt=""/>
-        <div className="mailContainer">
-            <a target="_blank" rel="noopener noreferrer" href="mailto:arthurdaynight@gmail.com">
-                <img  className="mailLogo" src="https://i.ibb.co/19qyFTX/Untitled-2.png" /> 
-                </a>
-                </div>
         </div>
         
         <div>
